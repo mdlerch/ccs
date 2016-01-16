8f013968ab54a30bbb326d65255c050628198ad8
Michael Lerch <mdlerch@gmail.com>
2016-01-16 15:40:51 -0700

    Working on demo version of clars

--- demo/ex_clars.R
+++ demo/ex_clars.R
@@ -1,3 +1,6 @@
+# need to prove that when x_j is "all in" i.e. when c_j = 0 i.e. there is no
+# more predictive power from x_j, there MUST be a new higher score
+
 library(lars)
 data(diabetes)
 x <- scale(diabetes$x)
@@ -8,21 +11,38 @@ x <- scale(x)
 n <- nrow(x)
 p <- ncol(x)
 
+set.seed(42)
 cost <- round(runif(p, 1, 10))
+price <- cost / max(cost)
 
 mu <- rep(0, n)
 
 r <- y - mu
 
+r_lm <- y - x %*% solve(t(x) %*% x) %*% t(x) %*% y
+
 C <- t(x) %*% r / sd(r) / (n - 1)
 
 CM <- max(abs(C))
+p <- price * CM
 
-S <- abs(C) - cost * CM / max(cost)
-cbind(S, C, cost)
+S <- abs(C) - p
+cbind(S, C, p, price, cost)
 
 idx <- which.max(S)
 SM <- S[idx]
 
 u <- x[ , idx]
+
+for (gam in seq(0, 100, 10))
+{
+    rp <- r - u * gam
+    cp <- t(x) %*% rp / sd(rp) / (n - 1)
+    Sp <- cp - p
+    print(which.max(Sp))
+}
+
+
+
+
 
