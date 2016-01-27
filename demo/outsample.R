df1ee15fbe9cb20d62f21a06ab9f47a0b42398d4
Michael Lerch <mdlerch@gmail.com>
2016-01-27 16:05:34 -0700

    Start of performance comparison

---
+++ demo/outsample.R
@@ -0,0 +1,20 @@
+library(lars)
+library(cosso)
+data(diabetes)
+x <- scale(diabetes$x)
+y <- diabetes$y
+maxk <- 15
+eps <- 1e-9
+x <- scale(x)
+n <- nrow(x)
+p <- ncol(x)
+
+set.seed(42)
+cost <- round(runif(p, 10, 100)) / 10
+
+
+cout <- clars(x, y)
+mout <- mlars(x, y)
+
+idx <- 4; sd(y - predictlars(cout, x, idx)); sd(y - predictlars(mout, x, idx))
+
