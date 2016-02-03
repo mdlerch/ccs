9da21788d01f621f832dbd5fd8903fb1ee9220d5
Michael Lerch <mdlerch@gmail.com>
2016-02-03 14:02:04 -0700

    Rename files to reflect current main func

--- R/clars.R
+++ R/clars.R
@@ -1,4 +1,4 @@
-clarsOLD <- function(x, y, cost, maxk = 1000, eps = 1e-6, trace = FALSE)
+clars <- function(x, y, cost, maxk = 50, eps = 1e-6, trace = FALSE, type = 1)
 {
     x <- scale(x)
     # variable setup
@@ -38,15 +38,14 @@ clarsOLD <- function(x, y, cost, maxk = 1000, eps = 1e-6, trace = FALSE)
     smax <- max(svec)
     j <- svec >= smax - eps
 
+    out <- data.frame(var = colnames(x))
+    out$status <- ifelse(Active, "In", "Out")
 
     while (nv < p & k < maxk)
     {
         k <- k + 1
 
         # 1. Find the next variable to add.
-
-        if (trace) { cat("\nIteration: "); cat(k); cat("\n"); }
-        #print(cbind(cvec, svec)) }
 
         # Equation 2.9
         Active <- Active | j
@@ -56,10 +55,6 @@ clarsOLD <- function(x, y, cost, maxk = 1000, eps = 1e-6, trace = FALSE)
         r <- y - mu
         cvec <- t(x) %*% r
         cmax <- max(abs(cvec[j]))
-
-        if (trace) { cat("selected: "); cat(colnames(x)[Active]); cat("\n") }
-        if (trace) { print(cbind(cvec[Active], svec[Active])) }
-        if (trace) { print(cbind(cvec[Inactive], svec[Inactive])) }
 
         # 2. Find unit-vector of equal projection.
         # Following equations 2.4 through 2.6
@@ -90,20 +85,73 @@ clarsOLD <- function(x, y, cost, maxk = 1000, eps = 1e-6, trace = FALSE)
             # Equation 2.11
             a <- t(x) %*% u
             # Equation 2.13
-            gammas <- apply(
+            gammas <- rep(0, p)
+            gammas[Inactive] <- apply(
                 cbind((cmax - cvec[Inactive]) / (AA - a[Inactive]),
                       (cmax + cvec[Inactive]) / (AA + a[Inactive])), 1, mingt0)
-            fgammas <- gammas[which(gammas > 0)]
-            best <- which.max((cmax - fgammas * AA) / cost[Inactive])
-            gamma <- fgammas[best]
-            j <- rep(FALSE, p)
-            j[which(cumsum(Inactive) == best)[1]] <- TRUE
+
+            # Type 1 find smallest gamma / cost
+            if (type == 1)
+            {
+                temp <- gammas / cost
+                temp[temp == 0] <- max(temp) + 1
+                newj <- which.min(temp)
+                gamma <- gammas[newj]
+                j[newj] <- TRUE
+            }
+            # Type 2 find largest correlation / projection
+            # if (type == 2)
+            # {
+            #     body
+            # }
+            # Type 3 drop variable if crossing 0
+            if (type == 3)
+            {
+                # start off like type 1
+                temp <- gammas * cost
+                temp[temp == 0] <- max(temp) + 1
+                newj <- which.min(temp)
+                gamma <- gammas[newj]
+                # find gamma tilde for each j
+                # populate with large values (for non-active variables)
+                gammaj <- -beta[k, Active] / (w * Signs)
+                j[newj] <- TRUE
+                # If there are any gammaj that will eventually cross
+                if (any(gammaj > 0))
+                {
+                    # get the first to cross
+                    temp <- gammaj
+                    temp[temp <= 0] <- max(temp) + 1
+                    outj <- which.min(temp)
+                    gamma.tilde <- gammaj[outj]
+                    if (gamma.tilde < gamma)
+                    {
+                        gamma <- gamma.tilde
+                        j[which(cumsum(Active) == outj)] <- FALSE
+                        j[which(cumsum(Inactive) == newj)] <- FALSE
+                        if (trace) cat("Variable crossing 0\n")
+                        nv <- nv - 1
+                    }
+                }
+            }
             if (trace)
             {
-                cat("gamma: "); cat(gamma)
-                cat("cmax/A: "); cat(cmax/AA)
-                cat("\nNew score: "); cat(smax - gamma * AA); cat("\nsmax: ")
-                cat(smax); cat("\nnextmax: "); cat(max(svec[Inactive])); cat("\n")
+                cat("\n\n")
+                cat("Iteration: ")
+                cat(k)
+                cat("\n")
+                out$status <- ifelse(Active, "In", "Out")
+                out$cvec <- cvec
+                out$cost <- cost
+                out$gamma <- gammas
+                out$gamrate <- gammas * cost
+                out$beta <- beta[k, ]
+                print(out)
+                print(gamma)
+                # cat("gamma: "); cat(gamma)
+                # cat("cmax/A: "); cat(cmax/AA)
+                # cat("\nNew score: "); cat(smax - gamma * AA); cat("\nsmax: ")
+                # cat(smax); cat("\nnextmax: "); cat(max(svec[Inactive])); cat("\n")
             }
             mu <- mu + gamma * u
             beta[k + 1, Active] <- beta[k, Active] + gamma * w * Signs
