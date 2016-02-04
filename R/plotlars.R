ab0ba07468828850dc8146a945fad1801d2095d9
Michael Lerch <mdlerch@gmail.com>
2016-02-03 17:48:40 -0700

    Add plotting function for lars output

---
+++ R/plotlars.R
@@ -0,0 +1,14 @@
+plotlars <- function(lars.object)
+{
+    betas <- lars.object$beta
+    k <- nrow(betas)
+    p <- ncol(betas)
+
+    plot(0, 0, type = 'n', xlim = c(0, k), ylim = c(min(betas), max(betas)))
+    for (v in 1:p)
+    {
+        lines(1:k, betas[ , v], col = v)
+        lines(1:k, betas[ , v], col = v, pch = 16)
+    }
+}
+
