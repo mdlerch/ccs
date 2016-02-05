a2d2cc7044f0902255252c347c37415a93511f0f
Michael Lerch <mdlerch@gmail.com>
2016-02-05 14:07:52 -0700

    Begin cross validation

---
+++ demo/ex_cross_validation.R
@@ -0,0 +1,31 @@
+library(lars)
+library(cosso)
+data(diabetes)
+x <- scale(diabetes$x)
+y <- diabetes$y
+maxk <- 15
+eps <- 1e-9
+x <- scale(x)
+n <- nrow(x)
+p <- ncol(x)
+
+set.seed(42)
+cost <- round(runif(p, 10, 100)) / 10
+
+ntest <- round(0.10 * n)
+test <- sample(1:n, ntest, replace = FALSE)
+train <- setdiff(1:n, test)
+
+
+
+cout2 <- clars(x, y, cost = cost, trace = TRUE, type = 3, maxk = 100)
+mout <- mlars(x, y)
+plotlars(cout2)
+
+clarsscore <- evalclars(cout2, x, y, cost)
+
+mlarsscore <- evalclars(mout, x, y, cost)
+
+plot(clarsscore$score ~ clarsscore$modelcost, type = "p", ylim = c(2500, 3600), col = "blue", pch = 16, cex = 2)
+points(mlarsscore$score ~ mlarsscore$modelcost, col = "red", pch = 16, cex = 2)
+
