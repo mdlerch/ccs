3ad52c1c4878616be5d2b2d828ac30b55e29a870
Michael Lerch <mdlerch@gmail.com>
2016-02-11 13:21:50 -0700

    Clean up R dir

--- R/aux.R
+++ R/aux.R
@@ -38,3 +38,32 @@ backsolvet <- function(r, x, k)
     x <- backsolve(r, x)
     drop(x[k:1,  ])
 }
+
+mingt0 <- function(x)
+{
+    if (sum(x > 0) > 0)
+    {
+        return(min(x[x > 0]))
+    }
+    return(0)
+}
+
+minlt0 <- function(x)
+{
+    if (sum(x < 0) > 0)
+    {
+        return(min(x[x < 0]))
+    }
+    return(0)
+}
+
+
+mindist <- function(x)
+{
+    if (sum(x > 0) != 0)
+    {
+        temp <- x[x != 0]
+        return(temp[which(abs(temp) == min(abs(temp)))])
+    }
+    return(0)
+}
