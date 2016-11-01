01cb04bc9177afcfd730d21a783275db3c4f65c6
Michael Lerch <mdlerch@gmail.com>
2016-10-31 19:55:06 -0600

    Add larscpp function

--- src/lars.cpp
+++ src/lars.cpp
@@ -1,7 +1,6 @@
 #include <RcppArmadillo.h>
 #include <stdio.h>
 
-
 using namespace arma;
 
 // [[Rcpp::export]]
@@ -11,11 +10,23 @@ arma::mat larscpp(arma::mat x, arma::mat beta, arma::vec y, int maxk) {
     int n = x.n_rows;
     int j;
 
-    arma::vec cvec(p);
-    arma::vec cveca(p);
-    arma::vec mu(n);
-    arma::uvec active(n);
-    arma::mat XA(1, 1);
+    double gam;
+
+    vec cvec(p);
+    vec cveca(p);
+    ivec cvecs(p);
+    uvec active(p);
+    uvec inactive(p);
+    mat XA(n, p);
+
+    vec mu = x * beta.row(0).t();
+    int idx;
+
+    for (int i = 0; i < p; ++i)
+    {
+        inactive(i) = i;
+    }
+
 
     int i = 0;
     uword nv = 0;
@@ -28,23 +39,80 @@ arma::mat larscpp(arma::mat x, arma::mat beta, arma::vec y, int maxk) {
         cveca = abs(cvec);
 
         double cmax = 0;
+        for (int k = 0; k < (p - nv); ++k)
+        {
+            idx = inactive(k);
+            if (cveca(idx) > cmax)
+            {
+                j = idx;
+                cmax = cveca(idx);
+            }
+            if (cvec(idx) > 0)
+            {
+                cvecs(idx) = 1;
+            } else {
+                cvecs(idx) = -1;
+            }
+        }
+        active(nv) = j;
+        nv++;
+
+        // build inactive
+        idx = 0;
+        bool skip;
         for (int k = 0; k < p; ++k)
         {
-            if (cveca(k) > cmax)
+            skip = false;
+            for (int h = 0; h < nv; ++h)
             {
-                j = k;
-                cmax = cveca(k);
+                if (k == active(h))
+                {
+                    skip = true;
+                    break;
+                }
+            }
+            if (!skip)
+            {
+                inactive(idx) = k;
+                idx++;
             }
         }
 
         // 2. find unit-vector of equal projection
-        active(nv) = j;
-        nv++;
-        vec signs = sign(cvec);
-        uvec activeshort = active.subvec(0, nv);
-        arma::mat XA = x.cols(activeshort);
+        for (int k = 0; k < nv; ++k)
+        {
+            XA.col(k) = x.col(active(k)) * cvecs(active(k));
+        }
+        mat gA = XA.cols(0, nv - 1).t() * XA.cols(0, nv - 1);
+        vec one(nv, fill::ones);
+        mat AA = 1 / sqrt(one.t() * pinv(gA) * one);
+        vec w = AA(0, 0) * (pinv(gA) * one);
+        vec u = XA.cols(0, nv - 1) * w;
 
+        // 3. increment
+        vec a = x.t() * u;
+        gam = cmax / AA(0, 0);
+        for (int k = 0; k < (p - nv); ++k)
+        {
+            idx = inactive(k);
+            double temp = (cmax - cvec(idx)) / (AA(0, 0) - a(idx));
+            if (temp > 0 & temp < gam)
+            {
+                gam = temp;
+            }
+            temp = (cmax + cvec(idx)) / (AA(0, 0) + a(idx));
+            if (temp > 0 & temp < gam)
+            {
+                gam = temp;
+            }
+        }
+
+        for (int k = 0; k < nv; ++k)
+        {
+            beta(i, active(k)) = beta(i - 1, active(k)) + gam * w(k) * cvecs(active(k));
+        }
+        mu = mu + gam * u;
     }
 
-    return XA;
+    return beta.rows(0, i);
 }
